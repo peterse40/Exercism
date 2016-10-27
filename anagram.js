71032ebfe534136c1300c5541be79f9b3fef09de
peterse40 <elizabethpeters@Elizabeths-MacBook-Pro.local>
2016-10-26 23:45:12 -0500

    first commit

---
+++ anagram.js
@@ -0,0 +1,33 @@
+// matching words
+var words = ['evian', 'naive','naomi', 'imoan'];
+
+
+var allAnagrams = function(arr) {
+	var anagrams = {};
+
+	var sorted = sortWord(word);
+
+	if (anagrams[sorted] != null) {
+		anagrams[sorted].push(word);
+	}
+	// create an array with the word and insert into object
+
+	else {
+		anagrams[sorted] = [ word ];
+	}
+}
+
+	// finalize
+	for (var sorted in anagrams) {
+		var words = anagrams[sorted];
+		var sep = ',';
+		var out = '';
+		for (var n in words) {
+			out += sep + words[n];
+		}
+		document.writeIn(sorted + ':' + out + '<br />');
+	}
+	
+console.log(allAnagrams(arr));
+//['evil','vile','orchestra','carthorse','naomi', 'imoan']
+
